4e0f664765670c5b16d3a0d65757089d1ae10c12
rhfovk <rhfovk@gmail.com>
2022-04-05 15:51:41 +0900

    Add: 문자 찾기 - 영현

---
+++ ch01-기본문제풀이/2주차/10. 문자 찾기/문자 찾기_영현.js
@@ -0,0 +1,33 @@
+// 최솟값 구하기
+
+// 답안
+function solution(sentence, alp) {
+  const result = sentence.split("").filter((e) => e === alp).length;
+  return result;
+}
+
+solution("COMPUTERPROGRAMMING", "R"); // 3
+
+// 선생님 답안
+function solution(s, t) {
+  let answer = 0;
+
+  for (let x of s) {
+    if (x === t) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
+let str = "COMPUTERPROGRAMMING";
+solution(str, "R"); // 3
+
+// 선생님 답안2
+function solution(s, t) {
+  let answer = s.split(t).length;
+  return answer - 1;
+}
+
+let str = "COMPUTERPROGRAMMING";
+solution(str, "R"); // 3
