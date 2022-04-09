f77c869bea347959d456ee9592e96ad669766b81
rhfovk <rhfovk@gmail.com>
2022-04-09 23:14:20 +0900

    Add: 대소문자 변환 - 영현

---
+++ ch01-기본문제풀이/3주차/13. 대소문자 변환/대소문자변환_영현.js
@@ -0,0 +1,52 @@
+// 대소문자 변환
+
+// 답안
+// map 안에 함수를 넣어보고 싶었음
+function solution(sentence) {
+  return sentence
+    .split("")
+    .map(function (a) {
+      if (a === a.toUpperCase()) {
+        return a.toLowerCase();
+      } else {
+        return a.toUpperCase();
+      }
+    })
+    .join("");
+}
+
+solution("giVeMeSoME"); //'GIvEmEsOme'
+
+// 두번째 답안
+// 삼항연산자로 하면 더 잘 보일까해서 바꿔봄
+function solution(sentence) {
+  return sentence
+    .split("")
+    .map(function (a) {
+      return a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase();
+    })
+    .join("");
+}
+
+solution("giVeMeSoME"); //'GIvEmEsOme'
+
+// 선생님 답안
+function solution(s) {
+  let answer = "";
+
+  for (let x of s) {
+    // 만약 소문자라면
+    if (x === x.toLowerCase()) {
+      // 대문자로 변경하여 저장
+      answer += x.toUpperCase();
+    } else {
+      answer += x.toLowerCase();
+    }
+  }
+  return answer;
+}
+
+let str = "giVeMeSoME";
+solution(str); // 'GIvEmEsOme'
+
+// 그놈의 아스키코드..패스
