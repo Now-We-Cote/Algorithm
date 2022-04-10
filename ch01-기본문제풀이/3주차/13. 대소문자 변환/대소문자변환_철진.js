415e9eac53a75b1d53d58165a878ed87e9f9e4fc
eden.ju <public.jin03@gmail.com>
2022-04-10 15:22:22 +0900

    feat: 대소문자 변환 풀이 - 철진

---
+++ ch01-기본문제풀이/3주차/13. 대소문자 변환/대소문자변환_철진.js
@@ -0,0 +1,23 @@
+// 대소문자 변환
+
+// 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로
+// 변환하여 출력하는 프로그램을 작성하세요.
+
+// 입력 - 첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+
+// 출력 - 첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+
+// 입력 예시 - StuDy
+
+// 출력 예시 - sTUdy
+
+{
+  const solution = (string) => {
+    return string
+      .split("")
+      .map((char) =>
+        char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+      )
+      .join("");
+  };
+}
