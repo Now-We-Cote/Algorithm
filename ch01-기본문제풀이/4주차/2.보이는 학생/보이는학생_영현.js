b84e295a32ba8815fdae5a9af9b9c497666a8c15
rhfovk <rhfovk@gmail.com>
2022-04-15 00:12:34 +0900

    Add: 보이는 학생 - 영현

---
+++ ch01-기본문제풀이/4주차/2.보이는 학생/보이는학생_영현.js
@@ -0,0 +1,20 @@
+// 큰 수 출력하기
+
+// 답안
+function solution(arr) {
+  // 맨 앞 학생은 보이니까 1부터 시작
+  let answer = 1;
+  // 제일 큰 학생의 기준을 처음에는 0번 idx로 잡음
+  let tall = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (tall < arr[i]) {
+      // 크다면 그 값을 제일 큰 학생으로 바꿔줌
+      tall = arr[i];
+      answer++;
+    }
+  }
+  return answer;
+}
+
+let arr = [130, 135, 148, 140, 145, 150, 150, 153];
+solution(arr);
