3d90d4e9d78b996f3b3faa582516e336ff28d261
rhfovk <rhfovk@gmail.com>
2022-04-26 00:13:01 +0900

    Wip: 가장짧은문자거리 - 영현

---
+++ ch01-기본문제풀이/5주차/4. 가장 짧은 문자거리/가장짧은문자거리_영현.js
@@ -0,0 +1,21 @@
+// 가장 짧은 문자거리
+
+// 답안
+
+const solution = (str, a) => {
+  let map = [];
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === a) {
+      map.push(i);
+    }
+  }
+  for (i = 0; i < str.length; i++) {
+    let dd = map.map((e) => Math.abs(e - i));
+    console.log(Math.min(...dd));
+  }
+};
+
+const str = "teachermode";
+const a = "e";
+
+solution(str, a);
