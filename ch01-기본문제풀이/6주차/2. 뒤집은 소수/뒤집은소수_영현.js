53ed9eccda3323662f95cc05358db3e569b4a916
rhfovk <rhfovk@gmail.com>
2022-05-08 15:28:21 +0900

    Add: 뒤집은 소수 - 영현

---
+++ ch01-기본문제풀이/6주차/2. 뒤집은 소수/뒤집은소수_영현.js
@@ -0,0 +1,30 @@
+// 뒤집은 소수
+
+// 답안
+
+const solution = (num, arr) => {
+  let answer = [];
+
+  // 소수 구하는 함수
+  const isPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (x of arr) {
+    const reverseNum = Math.floor(x.toString().split("").reverse().join(""));
+    if (reverseNum !== 1 && isPrime(reverseNum)) {
+      answer.push(reverseNum);
+    }
+  }
+
+  return answer;
+};
+
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+
+solution(9, arr); // '137'
