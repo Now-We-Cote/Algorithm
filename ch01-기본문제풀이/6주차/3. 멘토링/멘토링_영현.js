bf3f93a715522af30cee6e3b77b5e519daf4fe01
rhfovk <rhfovk@gmail.com>
2022-05-08 22:10:19 +0900

    Add: 멘토링 - 영현

---
+++ ch01-기본문제풀이/6주차/3. 멘토링/멘토링_영현.js
@@ -0,0 +1,45 @@
+// 멘토링
+
+// 답안
+
+const solution = (arr) => {
+  let answer = 0;
+  m = arr.length;
+  n = arr[0].length;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      let cnt = 0;
+      // 인덱스를 k로 돌림
+      for (let k = 0; k < m; k++) {
+        let pi,
+          pj = 0;
+
+        for (let s = 0; s < n; s++) {
+          if (arr[k][s] === i) {
+            pi = s;
+          }
+          if (arr[k][s] === j) {
+            pj = s;
+          }
+        }
+        if (pi < pj) {
+          cnt++;
+        }
+      }
+      if (cnt === m) {
+        answer++;
+      }
+    }
+  }
+
+  return answer;
+};
+
+let arr = [
+  [3, 4, 1, 2],
+  [4, 3, 2, 1],
+  [3, 1, 4, 2],
+];
+
+solution(arr); // '137'
