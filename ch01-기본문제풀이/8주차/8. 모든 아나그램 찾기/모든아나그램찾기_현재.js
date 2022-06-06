0f2babee23ad5c183f996aeb815e9cb5b7e27f2a
Casey <hjkdw9506@gmail.com>
2022-06-06 12:50:28 +0900

    Add : 8-8 문제 풀이 추가

--- ch01-기본문제풀이/8주차/8. 모든 아나그램 찾기/모든아나그램찾기_현재.js
+++ ch01-기본문제풀이/8주차/8. 모든 아나그램 찾기/모든아나그램찾기_현재.js
@@ -62,3 +62,40 @@
 
   solution(S, T);
 }
+
+{
+  // 위의 직접 푼 풀이는 이중 for문이기에 복잡도가 증가
+  // 아래 방식은 O(n)으로 푼 것
+  const solution = (s, t) => {
+    // key사이즈로 비교할 거임
+    // s =  t의 key에 value를 -1로 프리셋 해두고
+    // t와 일치하는게 있을때마다 +1을 해준다
+    // -1 + 1 = 0이니까 0 sH의 size가 0이되는 경우 t와 모든 값이 일치한다고 추측할 수 있다
+
+    let answer = 0;
+    let sH = new Map(); // S를 추종하는 map
+    for (let x of t) {
+      sH.set(x, (sH.get(x) || 0) - 1); // -1로 t 값 추종하게 초기화
+    }
+
+    let len = t.length - 1;
+    for (let i = 0; i < len; i++) {
+      // 기본 세팅  -처음에 필요한 idx 2 까지의 값 확인 (lt = 0용 세팅) -> 가운데 값 확인을 위해 거쳐가야한다
+      sH.set(s[i], (sH.get(s[i]) || 0) + 1);
+      if (sH.get(s[i]) === 0) sH.delete(s[i]);
+    }
+    let lt = 0; // 좌측 포인터
+    for (let rt = len; rt < s.length; rt++) {
+      //rt = 우측 포인터
+      sH.set(s[rt], (sH.get(s[rt]) || 0) + 1); // 우측이 추가될때마다 1 더해줌
+      if (sH.get(s[rt]) === 0) sH.delete(s[rt]); // sH을 세팅한다음에 0인 경우 = t에 있는게 겹치니 지워준다
+      if (sH.size === 0) answer++; // 0이 되면 모든 값이 t와 일치한다는 것으로 answer에 추가해준다
+      // size로 확인해주는 것이기 때문에 lt, rt 사이에 끼어있는 값들도 이미 확인한 것이 되어 문제 없음
+      sH.set(s[lt], (sH.get(s[lt]) || 0) - 1); // 옮겨간 lt 값 확인 (버리는 녀석이기 때문에 빼준다)
+      if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
+      lt++; // rt와 같은 라운드마다 올라가게 한다
+    }
+    return answer;
+  };
+  console.log(solution("bacacbcba", "abc"));
+}
