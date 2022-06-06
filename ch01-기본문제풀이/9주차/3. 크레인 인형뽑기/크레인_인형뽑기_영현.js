3a5f46fea5da82e699004aa71ea54e02ab8ae6a2
rhfovk <rhfovk@gmail.com>
2022-06-07 00:23:14 +0900

    Add: 크레인 인형뽑기 - 영현

---
+++ ch01-기본문제풀이/9주차/3. 크레인 인형뽑기/크레인_인형뽑기_영현.js
@@ -0,0 +1,36 @@
+// 크레인 인형뽑기
+
+// 답안
+
+const solution = (board, moves) => {
+  let answer = 0;
+  let stack = [];
+
+  for (let x of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][x - 1] !== 0) {
+        let tmp = board[i][x - 1];
+        board[i][x - 1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else {
+          stack.push(tmp);
+        }
+        break;
+      }
+    }
+  }
+
+  return answer;
+};
+const board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let moves = [1, 5, 3, 5, 1, 2, 1, 4];
+solution(board, moves);
