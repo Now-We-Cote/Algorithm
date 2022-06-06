635a7815387012a53b370cb7fcf174a967253d3e
Casey <hjkdw9506@gmail.com>
2022-06-06 10:29:23 +0900

    Add : 8-6 문제 풀이

--- ch01-기본문제풀이/8주차/5. 최대 매출/최대매출_현재.js
+++ ch01-기본문제풀이/8주차/5. 최대 매출/최대매출_현재.js
@@ -7,4 +7,36 @@
 
     return Math.max(...newArr);
   };
+
+  // sliding window 기법
+  // [a, b, c] 이렇게 먼저 합을 구하고, 그 다음 차례가 되면 [b, c, d]이렇게 보는 것
+  // [b, c, d]의 합을 구할 때 (a+b+c) + (d - a) 이런식으로 처리해주는 것을 sliding window 기법이라고 한다 => carousel같음
+  // 이렇게 하면 for 하나만 돌리면 되어서 시간복잠도가 O(n)이 된다
+
+  //! 위에 코드는 reduece가 answer 길이 만큼 반복하면서 콜백함수를 반복호출하는 구조이기 때문에 시간복잡도 O(n^2)입니다.
+  //위에 코드도 reduce가 i저점부터 k번을 반복문처럼 반복하기 때문에 결국에는
+  // for (let i = 0; i < arr.length; i++) {
+  //   let sum=0;
+  //   for(let j=i; j<i+k; j++){
+  //          sum+=arr[j]
+  // 과 같은 시간복잡도 O(n^2)입니다.
+  // 이런문제는 슬라이딩 윈도우로 하는 습관을 들여 놓는게 좋습니다.
+}
+
+{
+  // sliding window 사용
+  const solution = (n, k, arr) => {
+    let sum = 0;
+    let answer = 0;
+    for (let i = 0; i < k; i++) {
+      // 첫번째 합
+      sum += arr[i];
+    }
+    for (let i = k; i < n; i++) {
+      // 최대값 구하기
+      sum = sum + arr[i] - arr[i - k];
+      answer = Math.max(sum, answer);
+    }
+    return answer;
+  };
 }
---
+++ ch01-기본문제풀이/8주차/6. 학급 회장(해쉬)/학급회장_현재.js
@@ -0,0 +1,77 @@
+{
+  /* 해시함수
+장점
+중복을 제거할 수 있다.
+데이터 캐싱, 보안에 주로 사용된다.
+배열의 인덱스로 접근하기 때문에 삽입, 삭제 등의 연산이 빠르다.
+단점
+공간 복잡도가 커진다.
+충돌이 발생할 수 있다.
+- 충돌이 발생할 경우 시간 복잡도는 O(n)에 가까워진다.
+순서가 있는 배열에는 어울리지 않는다. */
+
+  // 해시는 객체로 처리하여 연산한다
+  const solution = (n, arr) => {
+    let obj = {
+      A: 0,
+      B: 0,
+      C: 0,
+      D: 0,
+      E: 0,
+    };
+
+    for (let char of arr) {
+      if (char in obj) {
+        obj[char] += 1;
+      }
+    }
+    const maxValue = Math.max(...Object.values(obj));
+    const maxKey = Object.keys(obj).find((key) => obj[key] === maxValue);
+
+    return maxKey;
+  };
+}
+
+{
+  // case 1.
+  // reduce를 가지고 객체 count 할 수 있다
+  //  만약 reduce() 함수 호출에서 initialValue를 제공한 경우, accumulator는 initialValue와 같고 currentValue는 배열의 첫 번째 값과 같습니다. initialValue를 제공하지 않았다면, accumulator는 배열의 첫 번째 값과 같고 currentValue는 두 번째와 같습니다.
+  const solution = (n, arr) => {
+    const result = arr.reduce((chars, char) => {
+      if (char in chars) {
+        chars[char]++;
+      } else chars[char] = 1;
+      return chars;
+    }, {}); // 이렇게 초기값을 객체로 제공해주면 객체로 반환한다!
+    const maxValue = Math.max(...Object.values(result));
+    const maxKey = Object.keys(result).find((key) => result[key] === maxValue);
+
+    return maxKey;
+  };
+}
+
+{
+  // case 2.
+  // 일반 객체를 사용할 때보다 map을 사용하는 경우가 효율이 더 좋다 (hash를 하라고 만들어놓은 것이기에 사용이 권장됨)
+  const solution = (n, arr) => {
+    let answer;
+    let max = 0;
+    const hash = new Map();
+    // map count
+    for (let char of arr) {
+      if (hash.has(char)) {
+        hash.set(char, hash.get(char) + 1);
+      } else {
+        hash.set(char, 1);
+      }
+    }
+    // find max value
+    for ([key, val] of hash) {
+      if (val > max) {
+        max = val;
+        answer = key;
+      }
+    }
+    return answer;
+  };
+}
