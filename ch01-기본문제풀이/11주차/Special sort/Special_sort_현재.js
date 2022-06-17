9bd9b0f0ad28e86d4781eb359b014bfc65d831e9
Casey <hjkdw9506@gmail.com>
2022-06-17 21:04:38 +0900

    add : special sort ->  버블 정렬로 재풀이

--- ch01-기본문제풀이/11주차/Special sort/Special_sort_현재.js
+++ ch01-기본문제풀이/11주차/Special sort/Special_sort_현재.js
@@ -11,3 +11,18 @@
   const arr = [1, 2, 3, -3, -2, 5, 6, -6];
   solution(arr);
 }
+
+{
+  // 버블 정렬로 구한 케이스
+  const solution = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i; j++) {
+        if (arr[j] > 0 && arr[j + 1] < 0) {
+          // 양의 정수는 뒤쪽으로 보내고, 음의 정수는 앞쪽으로 보낸다
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  };
+}
