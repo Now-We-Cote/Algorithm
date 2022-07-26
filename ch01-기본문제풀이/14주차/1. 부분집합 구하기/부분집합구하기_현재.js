6854a43043e2a993a2692290605f3558e13b9f19
Casey <hjkdw9506@gmail.com>
2022-07-26 22:14:43 +0900

    fix : 오타 수정

--- ch01-기본문제풀이/14주차/1. 부분집합 구하기/부분집합구하기_현재.js
+++ ch01-기본문제풀이/14주차/1. 부분집합 구하기/부분집합구하기_현재.js
@@ -1,30 +1,28 @@
 {
   const solution = (n) => {
-    let answer =[];
-    let chk = new Array(n+1).fill(0)
-    const DFS(v) => {
+    let answer = [];
+    let chk = new Array(n + 1).fill(0);
+    const DFS = (v) => {
       // DFS 잘 모르겠으면 무조건  if else
-      if(v === n + 1) {
+      if (v === n + 1) {
         // 종착점 =>  chk 배열에서  1 로 들어온 것만 출력
-        let char = ''
-        for(let i = 1; i <= n; i ++) {
-          if(chk[i] === 0) {
-            char += i+" "
+        let char = "";
+        for (let i = 1; i <= n; i++) {
+          if (chk[i] === 1) {
+            char += i + " ";
           }
         }
-        if(char.length > 0) answer.push(char.trim())
-
+        if (char.length > 0) answer.push(char.trim());
       } else {
         // 포함시키는 경우
-        chk[v] = 1
-        DFS(v + 1)
+        chk[v] = 1;
+        DFS(v + 1);
         // 안포함시키는 경우
-        chk[v] = 0
-        DFS(v + 1)
+        chk[v] = 0;
+        DFS(v + 1);
       }
-
-  }
-  DFS(1)
-  return answer
-  }
+    };
+    DFS(1);
+    return answer;
+  };
 }
