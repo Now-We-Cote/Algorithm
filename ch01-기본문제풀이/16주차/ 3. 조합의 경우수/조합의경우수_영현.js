31a1d26a355d649efbd7ba058dd230d5bfe92e8d
rhfovk <rhfovk@gmail.com>
2022-08-15 18:49:30 +0900

    Add: 조합의 경우수 메모리제이션 해답 추가 - 영현

--- ch01-기본문제풀이/16주차/ 3. 조합의 경우수/조합의경우수_영현.js
+++ ch01-기본문제풀이/16주차/ 3. 조합의 경우수/조합의경우수_영현.js
@@ -16,3 +16,23 @@ const solution = (n, r) => {
   return answer;
 };
 solution(5, 3);
+
+// 선생님 답안(메모리제이션)
+const solution = (n, r) => {
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+
+  const DFS = (n, r) => {
+    // dy배열에 저장해둔 값이 있으면
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) {
+      return 1;
+    } else {
+      return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r)); // DFS값을 return함과 동시에 dy[n][r]에 DFS값 저장
+    }
+  };
+  answer = DFS(n, r);
+
+  return answer;
+};
+solution(5, 3);
