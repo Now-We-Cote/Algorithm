79f4fefb5e0630d2e16bafd1f2f1d6041d56f300
rhfovk <rhfovk@gmail.com>
2022-10-30 19:50:03 +0900

    Add: 중복 문자 제거 - 영현

--- ch02-기본문제 2회독 풀이/4주차/중복문자제거/중복_문자_제거_영현.js
+++ ch02-기본문제 2회독 풀이/4주차/중복문자제거/중복_문자_제거_영현.js
@@ -3,12 +3,22 @@
 // 답안
 
 const solution = (str) => {
-  const isOdd = str.length % 2;
-  const midNum = str.length / 2;
-
-  return isOdd ? str.substr(midNum, 1) : str.substr(midNum - 1, 2);
+  let answer = [];
+  for (let i = 0; i < str.length; i++) {
+    if (answer.indexOf(str[i]) === -1) {
+      answer += str[i];
+    }
+  }
+  return answer;
 };
 
-const str = "study";
+const solution2 = (str) => {
+  const set = new Set(str);
+  const newArr = [...set].join("");
 
-console.log(solution(str));
+  return newArr;
+};
+
+const str = "ksekkset";
+
+console.log(solution2(str));
