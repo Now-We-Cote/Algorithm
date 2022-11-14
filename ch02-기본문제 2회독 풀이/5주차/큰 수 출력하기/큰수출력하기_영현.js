917f407b27012debe6fae3876c4a05ed4120c099
rhfovk <rhfovk@gmail.com>
2022-11-14 23:09:58 +0900

    Add: 보이는 학생 - 영현

--- ch02-기본문제 2회독 풀이/5주차/큰 수 출력하기/큰수출력하기_영현.js
+++ ch02-기본문제 2회독 풀이/5주차/큰 수 출력하기/큰수출력하기_영현.js
@@ -3,7 +3,7 @@
 // 답안
 function solution(arr) {
   let answer = [];
-  answer.push(arr[0]);
+  answer.push(arr[0]); // 첫번째 숫자는 무조건 출력
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < arr[i + 1]) {
       answer.push(arr[i + 1]);
