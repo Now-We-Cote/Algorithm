e104acfad5a3633ada7e8196a58f291b2f2e96f1
rhfovk <rhfovk@gmail.com>
2022-12-06 20:34:20 +0900

    Add: 자릿수의 합 - 영현

---
+++ ch02-기본문제 2회독 풀이/7주차/자릿수의 합/자릿수의_합_영현.js
@@ -0,0 +1,22 @@
+// 자릿수의 합
+
+// 답안
+const solution = (arr) => {
+  let answer;
+  let maxNum = 0;
+  arr.forEach((e) => {
+    const sum = [...e.toString()].reduce((a, b) => a + Number(b), 0);
+    if (sum > maxNum) {
+      maxNum = sum;
+      answer = e;
+    } else if (sum === maxNum) {
+      if (e > answer) answer = e;
+    }
+  });
+
+  return answer;
+};
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+
+solution(arr); // '137'
