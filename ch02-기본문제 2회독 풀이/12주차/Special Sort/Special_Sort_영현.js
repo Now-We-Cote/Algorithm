df563b6e097d73bc290e075fd41104aa886654db
rhfovk <rhfovk@gmail.com>
2023-01-16 00:10:32 +0900

    Add: Special Sort - 영현

---
+++ ch02-기본문제 2회독 풀이/12주차/Special Sort/Special_Sort_영현.js
@@ -0,0 +1,15 @@
+// Special Sort
+
+// 답안
+const solution = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+const arr = [1, 2, 3, -3, -2, 5, 6, -6];
+solution(arr);
---
+++ ch02-기본문제 2회독 풀이/12주차/삽입 정렬/삽입_정렬_영현.js
@@ -0,0 +1,15 @@
+// 삽입정렬
+
+// 답안
+const solution = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+const arr = [13, 5, 11, 7, 23, 15];
+solution(arr);
