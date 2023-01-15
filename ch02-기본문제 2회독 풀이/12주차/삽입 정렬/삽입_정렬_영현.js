b8195f7e15af8ad991d8f78eee6a23ef0dd1c5da
rhfovk <rhfovk@gmail.com>
2023-01-16 00:31:00 +0900

    Add: 삽입 정렬 - 영현

--- ch02-기본문제 2회독 풀이/12주차/삽입 정렬/삽입_정렬_영현.js
+++ ch02-기본문제 2회독 풀이/12주차/삽입 정렬/삽입_정렬_영현.js
@@ -2,14 +2,19 @@
 
 // 답안
 const solution = (arr) => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  for (let i = 1; i < arr.length; i++) {
+    let tmp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) {
+        arr[j + 1] = arr[j];
+      } else {
+        break;
       }
     }
+    arr[j + 1] = tmp;
   }
   return arr;
 };
-const arr = [13, 5, 11, 7, 23, 15];
+const arr = [11, 7, 5, 6, 10, 9];
 solution(arr);
