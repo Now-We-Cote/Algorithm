af1bd0d8ff0f1f3e83d48c5bf82a87617f90ae63
rhfovk <rhfovk@gmail.com>
2023-02-06 23:59:31 +0900

    Add: 이진트린순회 - 영현

---
+++ ch02-기본문제 2회독 풀이/15주차/이진트리순회(DFS)/이진트리순회_영현.js
@@ -0,0 +1,21 @@
+// 이진트리순회
+
+// 풀이
+const solution = (v) => {
+  let answer;
+
+  const DFS = (v) => {
+    if (v > 7) return;
+    else {
+      console.log("전위순회 >>>", v); // 일단 첫번째 부모 출력 -> 전위순회
+      DFS(v * 2); // 왼쪽 자식으로 이동
+      console.log("중위순회 >>>", v); // -> 중위순회
+      DFS(v * 2 + 1); // 오른쪽 자식으로 이동
+      console.log("후위순회 >>>", v); // -> 후위순회
+    }
+  };
+  DFS(v);
+  return answer;
+};
+
+solution(1);
