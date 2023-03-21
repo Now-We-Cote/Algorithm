ef564aa5896650b1e88657e7ded584876f665984
rhfovk <rhfovk@gmail.com>
2023-03-22 00:19:44 +0900

    Add: 조합수(메모이제이션 추가) - 영현

--- ch02-기본문제 2회독 풀이/18주차/조합수(메모이제이션)/조합수_영현.js
+++ ch02-기본문제 2회독 풀이/18주차/조합수(메모이제이션)/조합수_영현.js
@@ -11,3 +11,22 @@ const solution = (n, r) => {
 };
 
 solution(5, 3);
+
+// 메모이제이션 풀이
+const solution = (n, r) => {
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0)); // 최대로
+  const DFS = (n, r) => {
+    if (dy[n][r] > 0) return dy[n][r]; //2차원 배열에 입력했던 값이 있다면
+    if (n === r || r === 0) return 1;
+    else {
+      return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+    } //2차원에 배열에 값을 입력해둠
+  };
+
+  answer = DFS(n, r);
+
+  return answer;
+};
+
+solution(33, 19);
