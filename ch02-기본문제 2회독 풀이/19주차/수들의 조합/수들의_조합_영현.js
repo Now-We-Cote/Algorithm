9ee6e816e04117ba5b9c8310de68290832086984
rhfovk <rhfovk@gmail.com>
2023-05-08 23:36:48 +0900

    Add: 수열 추측하기 - 영현

---
+++ ch02-기본문제 2회독 풀이/19주차/수들의 조합/수들의_조합_영현.js
@@ -0,0 +1,18 @@
+// 수들의 조합
+
+// 풀이
+const solution = (N) => {
+  let answer;
+
+  const DFS = (L, n) => {
+    if (L === 1) {
+      answer = n;
+    } else {
+      DFS(L - 1, n * (L - 1));
+    }
+  };
+  DFS(N, N);
+  return answer;
+};
+
+solution(5);
---
+++ ch02-기본문제 2회독 풀이/19주차/수열 추측하기/수열_추측하기_영현.js
@@ -0,0 +1,49 @@
+// 수열 추측하기
+
+// 풀이
+const solution = (n, f) => {
+  let answer;
+  let flag = 0;
+  let dy = Array.from(Array(11), () => Array(11).fill(0)); //N이 최대 10까지라고 했기 때문에
+  let ch = Array.from({ length: n + 1 }, () => 0); //체크 배열
+  let p = Array.from({ length: n }, () => 0); //순열 저장
+  let b = Array.from({ length: n }, () => 0); //1331 처럼 조합값을 저장
+  console.log("ch", ch);
+
+  // 조합값을 구하는 함수
+  const combi = (n, r) => {
+    if (dy[n][r] > 0) return dy[n][r]; //2차원 배열에 입력했던 값이 있다면
+    if (n === r || r === 0) return 1;
+    else {
+      return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+    } //2차원에 배열에 값을 입력해둠
+  };
+
+  const DFS = (L, sum) => {
+    if (flag) return;
+
+    if (L === n && sum === f) {
+      answer = p.slice();
+      flag = 1;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  };
+
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+
+  DFS(0, 0);
+
+  return answer;
+};
+
+solution(4, 16);
